275beb7e1df22fee74e62b263e571f81e9342b64
piratike <kevinmachuca96@hotmail.com>
2022-01-13 10:24:25 +0000

    Done Part 9

---
+++ Sección 9/KarelRobot.js
@@ -0,0 +1,104 @@
+/* Code for Part 9 of Web Development Bootcamp */
+
+/*
+
+    First challenge
+    Move from one corner to the another one in 5x5 world
+
+    function main() {
+
+        move();
+        move();
+        move();
+        move();
+        turnLeft();
+        move();
+        move();
+        move();
+        move();
+    
+    }
+
+*/
+
+/*
+
+    Second Challenge
+    Go from one corner to another putting beepers and using functions
+
+    function main(){
+
+    doIt();
+
+    }
+
+    function doIt() {
+
+    moveOne();
+    moveOne();
+    moveOne();
+    moveOne();
+
+    }
+
+    function moveOne() {
+
+    putBeeper();
+    move();
+    turnLeft();
+    move();
+    turnRight();
+
+    }
+
+*/
+
+/*
+
+    Last Challenge
+    Create in a 5x5 world a pattern with beepers
+
+    function main(){
+        beepersRight();
+        goUpTurnLeft();
+        beepersLeft();
+        goUpTurnRight();
+        beepersRight();
+        goUpTurnLeft();
+        beepersLeft();
+        goUpTurnRight();
+        beepersRight();
+    }
+    
+    function goUpTurnRight() {
+        turnRight();
+        move();
+        turnRight();
+    }
+    
+    function goUpTurnLeft() {
+        turnLeft();
+        move();
+        turnLeft();
+    }
+    
+    function beepersRight() {
+        putBeeper();
+        move();
+        move();
+        putBeeper();
+        move();
+        move();
+        putBeeper();  
+    }
+    
+    function beepersLeft() {
+        move();
+        putBeeper();
+        move();
+        move();
+        putBeeper();
+        move();
+    }
+
+*/
