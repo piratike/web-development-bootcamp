e3b59953b9854bc6a754e7f02ba2979d173a0999
piratike <kevinmachuca96@hotmail.com>
2022-01-14 13:14:05 +0000

    Changed directories name and added the start of Part 10

---
+++ Part 10/loveCalculator.js
@@ -0,0 +1,18 @@
+/* Code for Part 10 of Web Development Bootcamp */
+
+prompt('What\'s your name?');
+prompt('What\'s their name?');
+
+var score = Math.round(Math.random() * 100);
+
+if(score > 70) {
+
+    alert('Your love score is ' + score + '%! Like Drogo and Khaleesi!');
+
+}
+
+else {
+
+    alert('Your love score is ' + score + '%');
+
+}
