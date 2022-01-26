82b110a9116e02c6327db693389df7da059fd4c4
piratike <kevinmachuca96@hotmail.com>
2022-01-26 19:10:33 +0000

    Started working with Exress

---
+++ Part 19 - ExpressJS with NodeJS/my-express-server/server.js
@@ -0,0 +1,16 @@
+/* First Express Server */
+
+const express = require('express');
+const app = express();
+
+app.get('/', function(request, response) {
+
+    response.send('<h1>Hello World!</h1>');
+
+});
+
+app.listen(3000, function() {
+
+    console.log('Server started');
+
+});
