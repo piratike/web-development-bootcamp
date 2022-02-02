44a43e621e3010f2226eea25f9a8637bc8753c23
piratike <kevinmachuca96@hotmail.com>
2022-02-02 20:59:01 +0000

    Started to work with EJS

--- Part 22 - EJS/todolist-v1/app.js
+++ Part 22 - EJS/todolist-v1/app.js
@@ -2,15 +2,18 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
 
 app.get('/', function(req, res) {
 
-    res.send('Hello World');
+    var today = new Date();
+    var currentDay = today.getDay();
+    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-})
+    res.render('list', {kindOfDay: daysOfWeek[currentDay]});
 
+});
 
 app.listen(3000, function() {
 
