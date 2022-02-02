cd28c3558eb96ec08520fddb0d2e63f97550d357
piratike <kevinmachuca96@hotmail.com>
2022-02-02 10:52:39 +0000

    Done with API use with express

--- Part 20 - APIs/Newsletter-Signup/app.js
+++ Part 20 - APIs/Newsletter-Signup/app.js
@@ -14,10 +14,54 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
-    console.log(firstName, lastName, email);
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed',
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const listId = '8ba886624f';
+    const url = 'https://us14.api.mailchimp.com/3.0/lists/' + listId;
+    const options = {
+        method: 'POST',
+        auth: 'piratike:4816761cbf67c3decdbd7683c18bb328-us14'
+    }
+
+    const request = https.request(url, options, function(response) {
+
+        if(response.statusCode === 200) {
+            res.sendFile(__dirname + '/success.html')
+        }
+
+        if(response.statusCode !== 200) {
+            res.sendFile(__dirname + '/failure.html')
+        }
+
+        response.on('data', function(data) {
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post('/failure', function(req, res) {
+
+    res.redirect('/');
 
 });
 
@@ -26,3 +70,6 @@ app.listen(3000, function() {
     console.log('Server is running!');
 
 });
+
+// API Key: 4816761cbf67c3decdbd7683c18bb328-us14
+// List ID: 8ba886624f
