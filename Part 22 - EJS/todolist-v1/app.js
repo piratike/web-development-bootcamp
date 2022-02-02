44b96b3e78e5a5ea03f9ccacbeb7c81e0e19da96
piratike <kevinmachuca96@hotmail.com>
2022-02-02 11:13:52 +0000

    Fixed gitignore, in Part 21 is the last time node_modules will appear

---
+++ Part 22 - EJS/todolist-v1/app.js
@@ -0,0 +1,19 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', function(req, res) {
+
+    res.send('Hello World');
+
+})
+
+
+app.listen(3000, function() {
+
+    console.log('Server is running!');
+
+});
