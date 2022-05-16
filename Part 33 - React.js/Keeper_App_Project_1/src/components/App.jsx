bf9b687e2884bbef76a41f035fc3de7194b63ce0
piratike <kevinmachuca96@hotmail.com>
2022-05-16 18:34:21 +0100

    Started with React JS, and the first part of the first project with it, the Keeper App

---
+++ Part 33 - React.js/Keeper_App_Project_1/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+
+    return (
+
+        <div>
+            <Header />
+            <Note />
+            <Footer/>
+        </div>
+
+    );
+
+}
+
+export default App;
---
+++ Part 33 - React.js/Keeper_App_Project_1/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+
+    return (
+
+        <footer>
+
+            <p>Copyright 2022</p>
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
